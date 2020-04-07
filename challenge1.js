00f96b26957f9620304a78a581da3aeed146874e
poushitaguha <pguha.kolkata@gmail.com>
2020-04-06 20:08:34 -0400

    First challenge completed

--- challenge1.js
+++ challenge1.js
@@ -5,37 +5,37 @@
 // Create a Corporate Array of objects
 let corporateArray = [
     {
-        bond: C1,
+        bond: "C1",
         term: 1.3,
         yield: 3.3
     },
     {
-        bond: C2,
+        bond: "C2",
         term: 2,
         yield: 3.8
     },
     {
-        bond: C3,
+        bond: "C3",
         term: 5.2,
         yield: 5.3
     },
     {
-        bond: C4,
+        bond: "C4",
         term: 9,
         yield: 6.2
     },
     {
-        bond: C5,
+        bond: "C5",
         term: 10.1,
         yield: 6.4
     },
     {
-        bond: C6,
+        bond: "C6",
         term: 16,
         yield: 9.3
     },
     {
-        bond: C7,
+        bond: "C7",
         term: 22.9,
         yield: 12.3
     }
@@ -44,37 +44,57 @@ let corporateArray = [
 // Create a Government array of objects
 let governmentArray = [
     {
-        bond: G1,
+        bond: "G1",
         term: 0.9,
         yield: 1.7
     },
     {
-        bond: G2,
+        bond: "G2",
         term: 2.3,
         yield: 2.3
     },
     {
-        bond: G3,
+        bond: "G3",
         term: 7.8,
         yield: 3.3
     },
     {
-        bond: G4,
+        bond: "G4",
         term: 12,
         yield: 5.5
     },
     {
-        bond: G5,
+        bond: "G5",
         term: 15.1,
         yield: 7.5
     },
     {
-        bond: G6,
+        bond: "G6",
         term: 24.2,
         yield: 9.8
     }
 ]
 
-let calculateYieldSpread = corporateArray.map(obj => {
+let yieldSpreadArray = corporateArray.map(corp => {
+    let outputObj = {};
+    let closestTerm = 999999;
+    let yield = 0;
+    let benchmark;
 
-})
+    governmentArray.forEach(gov => {
+        if (Math.abs(corp.term - gov.term) < closestTerm) {
+            closestTerm = Math.abs(corp.term - gov.term);
+            yield = corp.yield - gov.yield;
+            yield = yield.toFixed(2);
+            benchmark = gov.bond;
+        }
+    });
+
+    outputObj["bond"] = corp.bond;
+    outputObj["benchmark"] = benchmark;
+    outputObj["spread_to_benchmark"] = yield;
+    return outputObj;
+
+});
+
+console.log(yieldSpreadArray);
